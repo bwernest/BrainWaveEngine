6be7dbd1d44667ae4df492b9fa8132356bb73f39
bwernest <ernest.laprelleboucher@gmail.com>
2025-07-28 20:46:04 +0200

    Test update

---
+++ tests/asserts.py
@@ -0,0 +1,4 @@
+"""___Functions_____________________________________________________________"""
+
+def assertEqual(arg1: any, arg2: any) -> bool:
+    assert arg1 == arg2, f"Fail to match {arg1} and {arg2} !"
--- tests/test_initialisation.py
+++ tests/test_initialisation.py
@@ -1,14 +1,12 @@
 """___Modules_______________________________________________________________"""
 
 # BrainWaveEngine
-from ..engine import *
-
-# Python
-import pytest
+from .asserts import *
+from ..ImportBWE import *
 
 """___Function______________________________________________________________"""
 
 def test_layer():
     layer = Layer()
     layer.dense(2, 3)
-    assert layer.weights.size == [2, 3]
+    assertEqual(layer.weights.size, 6)
